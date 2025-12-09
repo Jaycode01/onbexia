37e39640bbfccbf2bb35463bced56e90914a1e4b
Jaycode01 <josephlamidi01@gmail.com>
2025-12-09 06:25:44 +0100

    feat(ui): add the section to show how users can use onbexia

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "./components/common/hero";
+import HowItWorks from "./components/common/how-it-works/how-it-works";
 
 function Home() {
   return (
     <>
       <Hero />
+      <HowItWorks />
     </>
   );
 }
